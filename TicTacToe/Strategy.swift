11d5e768dc30324e08adbcec3b045fca23246ff3
dotfold <jamesmcnamee@gmail.com>
2017-04-05 17:40:43 +1000

    empty strategy class

---
+++ TicTacToe/Strategy.swift
@@ -0,0 +1,14 @@
+//
+//  Strategy.swift
+//  TicTacToe
+//
+//  Created by James McNamee on 3/4/17.
+//  Copyright © 2017 James McNamee. All rights reserved.
+//
+
+import Foundation
+
+func checkWinner (for rows: [Int]) {
+    
+}
+
