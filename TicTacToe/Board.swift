a90c46bd2b4090a8cf2dc38af55a6749f117124e
dotfold <jamesmcnamee@gmail.com>
2017-04-05 17:38:30 +1000

    board structs / data models

---
+++ TicTacToe/Board.swift
@@ -0,0 +1,62 @@
+//
+//  Board.swift
+//  TicTacToe
+//
+//  Created by James McNamee on 3/4/17.
+//  Copyright © 2017 James McNamee. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Board: NSObject {
+    var currentPlayer: Player
+    
+    override init() {
+        self.currentPlayer = Player(type: PlayerType.x)
+    }
+    
+    init(players: Array<Player>) {
+        print("Created Board")
+        
+        // begin with first player
+        self.currentPlayer = players[0]
+    }
+}
+
+struct Position {
+    var x: Int
+    var y: Int
+}
+
+struct Cell {
+    var owner: Player?
+    var position: Position
+    var uiElement: UIButton
+    
+    init (uiElement: UIButton, position: Position) {
+        self.uiElement = uiElement
+        self.position = position
+    }
+    
+    init (owner: Player, uiElement: UIButton, position: Position) {
+        self.owner = owner
+        self.uiElement = uiElement
+        self.position = position
+    }
+}
+
+struct GameState {
+    let nilPlayer: Player = Player()
+    var board: [[Cell]] = [[], [], []]
+    var activePlayer: Player
+    
+    init (activePlayer: Player) {
+        self.activePlayer = activePlayer
+    }
+    
+    init (activePlayer: Player, board: [[Cell]]) {
+        self.activePlayer = activePlayer
+        self.board = board
+    }
+}
