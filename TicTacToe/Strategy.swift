31af5199ea491bc32657b59945f06c0b06e15824
dotfold <jamesmcnamee@gmail.com>
2017-04-05 23:55:58 +1000

    find a winner by checking all valid winning combinations of cells for the same owner

--- TicTacToe/Strategy.swift
+++ TicTacToe/Strategy.swift
@@ -8,7 +8,61 @@
 
 import Foundation
 
-func checkWinner (for rows: [Int]) {
+// search all combinations of winning cells to find if any of them contain all of the same player pieces
+func findWinner (board rows: [[Cell]]) -> Player {
     
+    let combinations: [[[Cell]]] = [
+        // rows
+        rows,
+        getColumns(board: rows),
+        getDiagonals(board: rows)
+    ]
+    
+        // flatten one level
+    let maybeWinner = combinations.reduce([], +)
+        // for each array of possible winning combinations
+        .reduce(Player(type: PlayerType.none), { (result, cells) -> Player? in
+            // found the winning player, don't check any more
+            if result?.type != PlayerType.none { return result }
+            
+            if areElementsEqual(array: cells, playerType: PlayerType.x) { return Player(type: PlayerType.x) }
+            if areElementsEqual(array: cells, playerType: PlayerType.o) { return Player(type: PlayerType.o) }
+            
+            return Player(type: PlayerType.none)
+        })
+    
+    print("any same? \(maybeWinner)")
+    return maybeWinner!
 }
 
+private func areElementsEqual (array: [Cell], playerType: PlayerType) -> Bool {
+    var equal = true
+    for cell in array {
+        if cell.owner?.type != playerType {
+            equal = false
+        }
+    }
+    return equal
+}
+
+// We want to get the matching ith element of each of the row arrays
+// in order to have an array of column arrays
+// Swifts native zip only allows for two sequences
+// so the second sequence is passed as a nested zipped sequence
+// http://stackoverflow.com/questions/28686647/how-can-i-run-through-three-separate-arrays-in-the-same-for-loop
+private func getColumns (board rows: [[Cell]]) -> [[Cell]] {
+    var result: [[Cell]] = []
+    for (r0, (r1, r2)) in zip(rows[0], zip(rows[1], rows[2])) {
+        let inner = [r0, r1, r2]
+        result.append(inner)
+    }
+    return result
+}
+
+// no easy functional operators for this that I'm aware of
+private func getDiagonals (board rows: [[Cell]]) -> [[Cell]] {
+    return [
+        [rows[0][0], rows[1][1], rows[2][2]],
+        [rows[0][2], rows[1][1], rows[2][0]]
+    ]
+}
