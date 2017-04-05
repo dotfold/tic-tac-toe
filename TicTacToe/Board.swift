44c3a5d1153240c60e2e1e3b5d61028c5fba6fa6
dotfold <jamesmcnamee@gmail.com>
2017-04-05 23:56:40 +1000

    expand the board struct to hold completed state

--- TicTacToe/Board.swift
+++ TicTacToe/Board.swift
@@ -53,6 +53,7 @@ struct GameState {
     let nilPlayer: Player = Player()
     var board: [[Cell]] = [[], [], []]
     var activePlayer: Player
+    var complete: Bool = false
     
     init (activePlayer: Player) {
         self.activePlayer = activePlayer
@@ -61,5 +62,11 @@ struct GameState {
     init (activePlayer: Player, board: [[Cell]]) {
         self.activePlayer = activePlayer
         self.board = board
+    }
+    
+    init (activePlayer: Player, board: [[Cell]], complete: Bool) {
+        self.activePlayer = activePlayer
+        self.board = board
+        self.complete = complete
     }
 }
