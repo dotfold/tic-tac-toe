6da100b03e3b6bf0dc510a3bd40523ecd905e826
dotfold <jamesmcnamee@gmail.com>
2017-04-08 13:25:28 +1000

    setup default ai game state

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -71,6 +71,16 @@ class ViewController: UIViewController {
             ]
         )
         
+        let defaultAIGameState = GameState(
+            activePlayer: Player(type: PlayerType.x),
+            board: [
+                [self.cells[0], self.cells[1], self.cells[2]],
+                [self.cells[3], self.cells[4], self.cells[5]],
+                [self.cells[6], self.cells[7], self.cells[8]]
+            ],
+            isAI: true
+        )
+        
         let defaultScoreboard = Scoreboard()
         
         // player 1 is always first for a new game set
